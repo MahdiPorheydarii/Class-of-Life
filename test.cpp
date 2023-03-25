77b007e5a40b74ecd39e6f167e26e3b438656a08
Imanghader <49917769+iman008@users.noreply.github.com>
2023-03-25 16:16:55 +0330

    test cell.h

--- test.cpp
+++ test.cpp
@@ -7,9 +7,17 @@ int main()
     string b = "AATCCGATCCGCCTTA";
     string c = "TTAGGCTAGGCGGAAT";
     Genome g(a, b, c);
-    cout << g.getDNA().s1 << " " << g.getDNA().s2 << endl;
-    g.longMut("CTT", "TGAGA");
-    cout << g.getDNA().s1 << " " << g.getDNA().s2 << endl;
+    // cout << g.getDNA().s1 << " " << g.getDNA().s2 << endl;
+    // g.longMut("CTT", "TGAGA");
+    // cout << g.getDNA().s1 << " " << g.getDNA().s2 << endl;
+    
+    Cell A;
+    vector<Genome> g_genomes;
+    g_genomes.push_back(g);
+    A.setCell(g_genomes);
+    for (auto x: A.getGz()){
+        cout<< x.getDNA().s1 << " " << x.getDNA().s2 << endl;
+    }
 
     return 0;
 }
