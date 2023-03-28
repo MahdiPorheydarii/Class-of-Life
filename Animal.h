4e9a43cedf3eb3fe4e788983c75d043a5aed4aac
Imanghader <49917769+iman008@users.noreply.github.com>
2023-03-28 17:24:32 +0330

    similarity changed and clone implemented

--- Animal.h
+++ Animal.h
@@ -10,34 +10,46 @@ class Animal : protected Cell
 
 public:
   friend double similarityPercentage(Animal A, Animal B) ;
+  friend Animal clone();
   friend bool operator==(Animal a, Animal b);
   friend Animal operator+(Animal a, Animal b);
   Animal clone();
   Animal();
 };
 
-Animal clone(){
+Animal Animal::clone(){
   Animal res;
-  return res;
+  if (gz.size()<10){
+    res.gz=gz;
+    return res;
+  } else {
+    res.gz=gz;
+    res.gz.pop_back();
+    srand(time(0));
+    int rand_id=rand()%gz.size();
+    res.gz.push_back(gz[rand_id]);
+  }
 }
 
 double similarityPercentage(Animal A, Animal B)
 {
-  double a = 0;
-  for (int i = 0; i < min(A.gz.size(),B.gz.size()); i++)
-  {
-    a += StringSim(A.gz[i].getDNA().s1, B.gz[i].getDNA().s1);
+  int num_sim=0;
+  for (int i=0; i<min(A.gz.size(), B.gz.size());i++){
+    if (A.gz[i].getDNA().s1==B.gz[i].getDNA().s1 and A.gz[i].getDNA().s2==B.gz[i].getDNA().s2){
+      ++num_sim;
+    }
   }
-  return a / ((double)(A.gz.size()+B.gz.size())/2);
+  return (double)num_sim / ((double)max(A.gz.size(), B.gz.size()))*100.0;
 }
 
 bool operator==(Animal a, Animal b)
 {
-  if (similarityPercentage(a, b) >= 70)
+  if (similarityPercentage(a, b) >= 70 and a.gz.size()==b.gz.size())
     return true;
   else return false;
 }
 
 Animal operator+(Animal a, Animal b)
 {
+
 }
